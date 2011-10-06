bcedb73f211de620c006af4f2ee23780b1842697
Ketoth Xupack <ketoth.xupack@gmail.com>
2011-10-07 02:02:57 +0700

    youji: fix for config
    Signed-off-by: Ketoth Xupack <ketoth.xupack@gmail.com>

--- youJi/src/main/java/org/ritsuka/youji/Config.java
+++ youJi/src/main/java/org/ritsuka/youji/Config.java
@@ -1,13 +1,11 @@
 package org.ritsuka.youji;
 
 import org.ritsuka.natsuo.yaconfig.ConfigKey;
+import org.ritsuka.natsuo.yaconfig.IConfigKey;
 import org.ritsuka.natsuo.yaconfig.IKeyVerifier;
 
 import java.net.URI;
 import java.util.List;
-
-//import org.ritsuka.natsuo.yaconfig.Foo;
-
 
 /**
  * Date: 10/1/11
@@ -17,27 +15,22 @@ public final class Config {
     private Config() {
     }
 
-    public static final ConfigKey<List<String>> INITIAL_ACCOUNTS =
-            new ConfigKey<List<String>>("youji.initial-accounts",
-                new IKeyVerifier.ListVerifier<String>(){});
+    public static final IConfigKey<List<String>> INITIAL_ACCOUNTS =
+            new ConfigKey<List<String>>("youji.initial-accounts")
+                .setVerifier(new IKeyVerifier.ListVerifier<String>() {});
 
-    public static final ConfigKey<Integer> RECONNECT_INTERVAL =
+    public static final IConfigKey<Integer> RECONNECT_INTERVAL =
             new ConfigKey<Integer>("youji.reconnect.interval");
 
-    public static final ConfigKey<Integer> MUC_REJOIN_INTERVAL =
+    public static final IConfigKey<Integer> MUC_REJOIN_INTERVAL =
             new ConfigKey<Integer>("youji.muc.rejoin.interval");
 
-    public static final ConfigKey<Integer> MUC_REJOIN_MAXINTERVAL =
+    public static final IConfigKey<Integer> MUC_REJOIN_MAXINTERVAL =
             new ConfigKey<Integer>("youji.muc.rejoin.maxinterval");
 
-    public static final ConfigKey<URI> DB_LOGS =
+    public static final IConfigKey<URI> DB_LOGS =
             new ConfigKey<URI>("youji.db.logs");
 
-    public static final ConfigKey<URI> DB_CONFIGS =
+    public static final IConfigKey<URI> DB_CONFIGS =
             new ConfigKey<URI>("youji.db.config");
-
-/*
-    public static final ConfigKey<Foo> TEST =
-            new ConfigKey<Foo>("test", new ReflectConstructor<Foo>(){});
-*/
 }
